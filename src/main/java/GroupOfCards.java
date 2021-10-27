6832baf4314af73c043723f419c33a68940cf128
Marco <Marco@DESKTOP-SPLA3EH.myfiosgateway.com>
2021-10-27 01:27:07 -0400

    Stubs and Structure

---
+++ src/main/java/GroupOfCards.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marco
+ */
+public class GroupOfCards {
+    Card[] cards;
+    int currentSize;
+    
+    static void addCard(Card card){
+        
+    }
+    
+    static void display(){
+        
+    }
+    
+}
